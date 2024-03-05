2109eb3ea7338ad86103130092e57b06362e6796
obinnainfinity <150717679+obinnainfinity@users.noreply.github.com>
2024-03-05 13:18:53 -0800

    we are here

---
+++ Assets/JS/script.js
@@ -0,0 +1,14 @@
+
+// nav hamburger menu
+// const menuBtn = document.querySelector('#HamburgerMenu');
+// const togglerContainer = document.querySelector('.navItems')
+
+// function myDrop() {
+//   if (togglerContainer.classList.contains("showclass")) {
+//     togglerContainer.classList.remove("showclass");
+//   }
+//   else {
+//     togglerContainer.classList.add("showclass")
+//   }
+// }
+// menuBtn.addEventListener('click', myDrop);
